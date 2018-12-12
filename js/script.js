44e7a7556138d394f8ef8a81683d94cc1fb00472
Joanna <j.puno0909@gmail.com>
2018-12-11 23:01:18 -0700

    a

--- js/script.js
+++ js/script.js
@@ -82,4 +82,20 @@ $(document).ready(function() {
     });
 
 
+    var modal = document.querySelector("#modal");
+var modalOverlay = document.querySelector("#modal-overlay");
+var closeButton = document.querySelector("#close-button");
+var openButton = document.querySelector("#open-button");
+
+closeButton.addEventListener("click", function() {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
+
+openButton.addEventListener("click", function() {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
+
+
 
